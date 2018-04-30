beec3df0b0b7e4a38d98ecf1c35b55f8d3d1c50a
stfnhmplr <stfnhmplr@gmail.com>
2018-04-30 15:21:15 +0200

    added build-essential as default dependency

--- node-js/commands.sh
+++ node-js/commands.sh
@@ -2,7 +2,6 @@ if [ $(uname -m) == "armv7l" ]; then
     echo "Paketquellen anpassen"
 	curl -sL https://deb.nodesource.com/setup_8.x | sudo -E bash -
 	sudo apt-get install -y nodejs
-	sudo apt-get install -y build-essential
 elif [ $(uname -m) == "armv6l" ]; then
     wget https://nodejs.org/dist/v8.9.4/node-v8.9.4-linux-armv6l.tar.xz
 	tar -xvf node-v8.9.4-linux-armv6l.tar.xz
