98af193f2064e4aa570a987786591489ab3d9bb2
stfnhmplr <stfnhmplr@gmail.com>
2018-05-04 20:58:59 +0200

    added nginx with php

---
+++ nginx-php/commands.sh
@@ -0,0 +1,16 @@
+sudo apt-get install nginx php-fpm
+
+sudo sed -i -e 's/index index.html index.htm/index index.php index.html index.htm/g' /etc/nginx/sites-enabled/default
+
+sudo sed -i -e '56s/#//g' /etc/nginx/sites-enabled/default
+sudo sed -i -e '57s/#//g' /etc/nginx/sites-enabled/default
+sudo sed -i -e '60s/#//g' /etc/nginx/sites-enabled/default
+sudo sed -i -e '63s/#//g' /etc/nginx/sites-enabled/default
+
+cat > ~/index.php << EOF
+        <?php echo phpinfo(); ?>
+EOF
+
+sudo mv ~/index.php /var/www/html/index.php
+
+sudo /etc/init.d/nginx reload
---
+++ nginx-php/description.md
@@ -0,0 +1,3 @@
+Dieses Paket beinhaltet den schnellen Nginx HTTP Server und PHP-FPM.
+
+Nach der Installation lassen sich im Verzeichnis `/var/www/html` sowohl HTML als auch PHP Dateien ablegen. Diese sind anschließend über den Browser erreichbar.
