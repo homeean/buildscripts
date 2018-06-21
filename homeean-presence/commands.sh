3ef55b7810452f794e8d8e6b4aa1e1764a524db2
stfnhmplr <stfnhmplr@gmail.com>
2018-06-21 21:55:27 +0200

    added homeean-presence as package

---
+++ homeean-presence/commands.sh
@@ -0,0 +1,44 @@
+echo "Installiere homeean-presence"
+sudo apt-get install bluetooth bluez libbluetooth-dev libudev-dev
+sudo setcap cap_net_raw+eip $(eval readlink -f `which node`)
+sudo npm install -g homeean-presence --unsafe-perm
+mkdir -p ~/.homeean-presence
+cat > ~/.homeean-presence/config.json << EOF
+{
+    "interval": 30,
+    "threshold": 180,
+    "port": 3000,
+    "webhooks": {
+        "absent": "",
+        "present": ""
+    },
+    "persons": [
+        {
+            "name": "firstname",
+            "uuid": "",
+            "ip": ""
+        }
+    ]
+}
+
+EOF
+cat > homeean-presence.service << EOF
+[Unit]
+Description=homeean presence detection
+After=syslog.target network-online.target
+
+[Service]
+Type=simple
+User=pi
+ExecStart=DEBUG=homeean-presence $(which homeean-presence)
+Restart=on-failure
+RestartSec=10
+KillMode=process
+
+[Install]
+WantedBy=multi-user.target
+EOF
+sudo mv homeean-presence.service /etc/systemd/system/homeean-presence.service
+sudo systemctl daemon-reload
+sudo systemctl enable homeean-presence
+sudo systemctl start homeean-presence
---
+++ homeean-presence/description.md
@@ -0,0 +1,5 @@
+homeean-presence bietet eine einfache Möglichkeit zur Anwesenheitserkennung im Smart Home. Für jede Person kann ein IP und ein BLE Device getrackt werden. Zusätzlich kann der Anwesenheitstatus über einen Webhook gesetzt werden.
+
+Ändert sich der Status (mindestens eine Person ist anwesend oder keine Person ist anwesend), kann ein Webhook ausgelöst werden.
+
+Nach der Installation muss eine Konfigurationsdatei (`~/.homeean-presence/config.json`) bearbeitet werden.
