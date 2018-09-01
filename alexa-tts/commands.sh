331adfc87efd2359dbf80f85cdd48441b21850f4
stfnhmplr <stfnhmplr@gmail.com>
2018-09-01 21:30:49 +0200

    go back to user dir

--- alexa-tts/commands.sh
+++ alexa-tts/commands.sh
@@ -1,4 +1,5 @@
+sudo apt-get install php-curl -y
 cd /var/www/html
 sudo wget https://www.intelligentes-haus.de/wp-content/uploads/2018/06/alexa_tts.zip
 sudo unzip alexa_tts.zip
-sudo apt-get install php-curl -y
+cd ~
