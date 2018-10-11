14e4cf97fc576555ffc313f03bbc2c194e5c4f9f
stfnhmplr <stfnhmplr@gmail.com>
2018-10-11 15:58:59 +0200

    switch to homebridge-config-ui-x

--- homebridge/commands.sh
+++ homebridge/commands.sh
@@ -1,6 +1,6 @@
 echo "Homebridge mit config-ui installieren"
 sudo apt-get install -y libavahi-compat-libdnssd-dev
-sudo npm install -g homebridge homebridge-config-ui --unsafe-perm
+sudo npm install -g homebridge homebridge-config-ui-x --unsafe-perm
 echo "Autostart einrichten"
 cat > homebridge.service << EOF
 [Unit]
@@ -32,15 +32,17 @@ cat > ~/.homebridge/config.json << EOF
   "description": "This is an example configuration file for the homebridge-homee plugin",
   "hint": "Always paste into jsonlint.com validation page before starting your homebridge, saves a lot of frustration",
   "platforms": [
-    {
-      "platform": "config",
-      "name": "Config",
-      "port": 8080,
-      "log": "/var/log/daemon.log",
-      "restart": "sudo systemctl restart homebridge.service",
-      "base": "/usr/lib/node_modules"
-    }
-  ],
+        {
+            "platform": "config",
+            "name": "Config",
+            "port": 8080,
+            "restart": "sudo -n systemctl restart homebridge",
+            "log": {
+                "method": "systemd",
+                "service": "homebridge"
+            }
+        }
+    ],
   "accessories":[]
 }
 EOF
