4651f94963a141c0e2e7aad799cc9fc91ae6fd48
stfnhmplr <stfnhmplr@gmail.com>
2018-12-14 11:34:56 +0100

    added homeean-dashboard to packages

---
+++ homeean-dashboard/commands.sh
@@ -0,0 +1,55 @@
+echo "homeean dashboard installieren"
+
+if ! [ -x "$(command -v composer)" ]; then
+  echo "composer not found, installing composer"
+  curl -sS https://getcomposer.org/installer | php
+  sudo mv composer.phar /usr/local/bin/composer
+fi
+
+if [ ! -e /var/www/dashboard ]; then
+    git clone https://github.com/homeean/dashboard.git
+    cd dashboard
+    echo "installing dependencies"
+    composer install --no-interaction --quiet
+    mv example-config.json config.json
+    mv example-style.css style.css
+    mv example-script.js script.js
+    cd ..
+    sudo mv dashboard /var/www/dashboard
+
+    echo "create webserver config"
+
+cat > homeean_dashboard << EOF
+server {
+    listen 8022;
+
+    root /var/www/dashboard;
+
+    index index.html index.htm index.php;
+
+    server_name homeean_dashboard;
+
+    location / {
+        try_files \$uri \$uri/ /index.php\$is_args\$args;
+    }
+
+    location ~ \.php$ {
+        include snippets/fastcgi-php.conf;
+        fastcgi_pass unix:/var/run/php/php7.0-fpm.sock;
+    }
+}
+EOF
+    sudo mv homeean_dashboard /etc/nginx/sites-available
+    sudo ln -s /etc/nginx/sites-available/homeean_dashboard /etc/nginx/sites-enabled/homeean_dashboard
+
+    echo "restart nginx"
+    sudo service nginx restart
+else
+    echo "homeean dashboard already installed, fetching updates"
+    cd /var/www/dashboard
+    git pull
+    echo "updating dependencies"
+    composer update --no-interaction --quiet
+fi
+
+cd ~
---
+++ homeean-dashboard/description.md
@@ -0,0 +1,9 @@
+Das Paket bietet Dir einen Rahmen um das homee Dashboard (oder die ganze Webapp) anzupassen.
+
+Für einen Schnellstart wird eine CSS und JS Datei mitgeliefert. Diese können beliebig angepasst werden. Du musst nur noch die URL deines homees in eine Konfigurationsdatei (`config.json`) eintragen. Alle Dateien findest du im Installationsverzeichnis `/var/www/dashboard`
+
+Das homeean Dashboard ist erreichbar unter `http://raspberrypi.local:8022`. Alternativ zum Hostnamen kann auch die IP-Adresse verwendet werden. Nach dem ersten Login muss die Seite aktualisiert werden, damit die Widgets sichtbar sind.
+
+Das Paket ist updatefähig, deine individuellen Einstellungen werden nicht überschrieben.
+
+Mehr zum Thema gibt es in der homee-Community: [https://community.hom.ee/t/wie-sich-das-homee-dashboard-beliebig-anpassen-laesst/16984](https://community.hom.ee/t/wie-sich-das-homee-dashboard-beliebig-anpassen-laesst/16984)
