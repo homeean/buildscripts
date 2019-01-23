27008003a22b47bf130f7ade62ad5c2a083dc576
stfnhmplr <stfnhmplr@gmail.com>
2019-01-23 22:12:22 +0100

    updated default nginx config

--- nginx-php/commands.sh
+++ nginx-php/commands.sh
@@ -1,16 +1,32 @@
 echo -e "${COLOR_ORANGE}Installiere NGINX und PHP${FORMAT_END}"
 sudo apt-get install nginx php-fpm -y
 
-sudo sed -i -e 's/index index.html index.htm/index index.php index.html index.htm/g' /etc/nginx/sites-enabled/default
+cat > ~/default << EOF
+server {
+	listen 80 default_server;
+    listen [::]:80 default_server;
 
-sudo sed -i -e '56s/#//g' /etc/nginx/sites-enabled/default
-sudo sed -i -e '57s/#//g' /etc/nginx/sites-enabled/default
-sudo sed -i -e '60s/#//g' /etc/nginx/sites-enabled/default
-sudo sed -i -e '63s/#//g' /etc/nginx/sites-enabled/default
+    root /var/www/html;
+    index index.php index.html index.htm index.nginx-debian.html;
+
+	server_name _;
+
+	location / {
+        try_files \$uri \$uri/ =404;
+    }
+
+	location ~ \.php$ {
+        include snippets/fastcgi-php.conf;
+        fastcgi_pass unix:/run/php/php7.0-fpm.sock;
+    }
+}
+EOF
+
+sudo mv -f ~/default /etc/nginx/sites-enabled/default
 
 cat > ~/index.php << EOF
         <?php echo phpinfo(); ?>
 EOF
 
-sudo mv ~/index.php /var/www/html/index.php
+sudo mv -f ~/index.php /var/www/html/index.php
 sudo /etc/init.d/nginx reload
