966a45c832290130e66508aa589f9d6a51e5e5e7
stfnhmplr <stfnhmplr@gmail.com>
2019-09-24 19:39:33 +0200

    removed unattended upgrade config

--- _footer/commands.sh
+++ _footer/commands.sh
@@ -1,6 +1,3 @@
-# config for unattended upgrades
-sudo dpkg-reconfigure -priority=low unattended-upgrades
-
 # change password after homeean installation
 read -p "Aus Sicherheitsgründen sollte das Standard-Passwort geändert werden. Möchtest Du es jetzt ändern? [y/n]: "
 [ "$REPLY" != "y" ] || passwd
